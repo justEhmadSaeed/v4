4f731047dd04c8e550234bf70b131b51f3673b55
Ehmad <justEhmadSaeed@gmail.com>
2020-06-04 00:53:01 +0500

    Update Social Links & keywords

--- src/config.js
+++ src/config.js
@@ -1,38 +1,36 @@
 module.exports = {
-  siteTitle: 'Brittany Chiang | Software Engineer',
+  siteTitle: 'Ehmad Saeed | Developer & Designer',
   siteDescription:
-    'Brittany Chiang is a software engineer based in Boston, MA who specializes in building (and occasionally designing) exceptional websites, applications, and everything in between.',
+    'Ehmad is a Computer Scientist based in Pakistan, exploring (and occasionally designing) exceptional websites, applications, and designs.',
   siteKeywords:
-    'Brittany Chiang, Brittany, Chiang, bchiang7, software engineer, front-end engineer, web developer, javascript, northeastern',
-  siteUrl: 'https://brittanychiang.com',
+    'Ehmad Saeed, Ehmad, Saeed, bchiang7, software engineer, front-end engineer, web developer, javascript, pakistan',
+  siteUrl: 'https://ehmadsaeed.me',
   siteLanguage: 'en_US',
-  googleAnalyticsID: 'UA-45666519-2',
-  googleVerification: 'DCl7VAf9tcz6eD9gb67NfkNnJ1PKRNcg8qQiwpbx9Lk',
-  name: 'Brittany Chiang',
-  location: 'Boston, MA',
-  email: 'brittany.chiang@gmail.com',
-  github: 'https://github.com/bchiang7',
-  twitterHandle: '@bchiang7',
+  name: 'Ehmad Saeed',
+  location: 'Lahore, PK',
+  email: 'justehmadsaeed@gmail.com',
+  github: 'https://github.com/justEhmadSaeed',
+  twitterHandle: '@justEhmadSaeed',
   socialMedia: [
     {
       name: 'GitHub',
-      url: 'https://github.com/bchiang7',
+      url: 'https://github.com/justEhmadSaeed',
     },
     {
       name: 'Linkedin',
-      url: 'https://www.linkedin.com/in/bchiang7',
+      url: 'https://www.linkedin.com/in/justEhmadSaeed',
     },
-    {
-      name: 'Codepen',
-      url: 'https://codepen.io/bchiang7',
-    },
+    // {
+    //   name: 'Codepen',
+    //   url: 'https://codepen.io/bchiang7',
+    // },
     {
       name: 'Instagram',
-      url: 'https://www.instagram.com/bchiang7',
+      url: 'https://www.instagram.com/justEhmadSaeed',
     },
     {
       name: 'Twitter',
-      url: 'https://twitter.com/bchiang7',
+      url: 'https://twitter.com/justEhmadSaeed',
     },
   ],
 
